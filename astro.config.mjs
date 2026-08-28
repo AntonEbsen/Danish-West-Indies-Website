3f800b16ca2c67a3b0448646ccba117cd54c6981
AntonEbsen <anton.ebsen@gmail.com>
2026-08-28 13:28:36 +0200

    feat: Implement i18n architecture for English and Danish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danishwestindies.org',
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "da"],
+    routing: {
+        prefixDefaultLocale: false
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  en: 'English',
+  da: 'Dansk',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.history': 'History',
+    'nav.memorial': 'Memorial',
+    'nav.resources': 'Resources',
+    'nav.thesis': 'Thesis',
+    'nav.about': 'About',
+  },
+  da: {
+    'nav.history': 'Historie',
+    'nav.memorial': 'Mindesmærke',
+    'nav.resources': 'Ressourcer',
+    'nav.thesis': 'Speciale',
+    'nav.about': 'Om Projektet',
+  },
+} as const;
